117b3303a4c56a61a25c54ccf4b372afede4342f
Alejandro, Morales <alejandro.morales@sky.uk>
2026-06-16 13:11:23 +0200

    Adding prettier and run formatter

--- graph/customer/customer.js
+++ graph/customer/customer.js
@@ -1,28 +1,26 @@
-import { readFileSync } from "fs";
-import { buildSchema } from "graphql";
+import { readFileSync } from 'fs';
+import { buildSchema } from 'graphql';
 
 let customers = [];
 
-const typeDefs = buildSchema(
-  readFileSync("./graph/customer/customer.graphql", "utf8")
-);
+const typeDefs = buildSchema(readFileSync('./graph/customer/customer.graphql', 'utf8'));
 
 const resolvers = {
   Query: {
-    getCustomer: (_, { email } ) => customers.find((customer) => customer.email === email),
-    getCustomersList: (_, ) => customers
+    getCustomer: (_, { email }) => customers.find((customer) => customer.email === email),
+    getCustomersList: (_) => customers,
   },
   Mutation: {
-    addCustomer: (_, { customer } ) => {
+    addCustomer: (_, { customer }) => {
       customers.push(customer);
       return customers;
     },
-    removeCustomer: (_, { email } ) => {
-      if (customers.length === 0) throw Error("Customers array is empty.");
-      customers = customers.filter(customer => customer?.email !== email);
+    removeCustomer: (_, { email }) => {
+      if (customers.length === 0) throw Error('Customers array is empty.');
+      customers = customers.filter((customer) => customer?.email !== email);
       return customers;
-    }
-    }
+    },
+  },
 };
 
 export { typeDefs, resolvers };
